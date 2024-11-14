1c666c85da0fce420c5f3661bdb827575e0b64f8
Lyubomir Yatanski <l.yatanski@gmail.com>
2024-11-14 08:56:49 +0200

    Subscriber (#6)
    * add subscriber for HSS
    
    * run test after hss
    
    * make python wait longer for hss
    
    * split core provisioning into multiple containers
    
    * fix yaml white space consistency
    
    * add subscriber configurability
    
    * expose env variable to python process
    
    * fix name mismatch
    
    * add time for message processing

--- test.py
+++ test.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
+
 import os
 import sys
 import time
 import logging
 import tinyWRAP as s
-
-cscf = "192.168.100.101"
-domain = "ims.mnc01.mcc268.3gppnetwork.org"
 
 class SipCallback(s.SipCallback):
     def __init__(self, imsi, msisdn, call):
@@ -96,70 +94,39 @@ class SipCallback(s.SipCallback):
         return 0
 
 
-class DDebugCallback(s.DDebugCallback):
-    def __init__(self):
-        super().__init__()
-        logging.basicConfig(level=logging.INFO, format="%(levelname)8s %(name)s:%(message)s")
-        self.log = logging.getLogger("main")
+def gen(cscf, domain, transport="udp"):
+    def run_ue(imsi, msisdn, Ki, opc, bind=None, ipsec=None, call=None):
+        sip = s.SipStack(SipCallback(imsi, msisdn, call),
+                         realm_uri=domain,
+                         impi_uri=f"{imsi}@{domain}",
+                         impu_uri=f"sip:{imsi}@{domain}")
+        sip.setProxyCSCF(cscf, 5060, transport, "ipv4")
 
-    def __enter__(self):
-        return self
+        sip.setLocalPort(5060, transport)
+        if bind: sip.setLocalIP(bind, transport)
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
+        # Milenage parameters
+        sip.setAMF("8000")
+        sip.setPassword(Ki)
+        sip.setOperatorIdConcealed(opc)
 
-    def OnDebugInfo(self, msg):
-        self.log.info(msg)
-        return 0
+        if(ipsec):
+            if(not sip.setIPsecPlugin(ipsec)): sys.exit(1)
+            sip.setIPSecSecAgree(True)
+            sip.setIPSecParameters("hmac-md5-96", "null", "trans", "esp")
 
-    def OnDebugWarn(self, msg):
-        self.log.warning(msg)
-        return 0
+        sip.start()
 
-    def OnDebugError(self, msg):
-        self.log.error(msg)
-        return 0
-
-    def OnDebugFatal(self, msg):
-        self.log.critical(msg)
-        return 0
-
-
-def run_ue(imsi, msisdn, Ki, opc, bind=None, ipsec=None, call=None):
-    sip = s.SipStack(SipCallback(imsi, msisdn, call),
-                     realm_uri=domain,
-                     impi_uri=f"{imsi}@{domain}",
-                     impu_uri=f"sip:{imsi}@{domain}")
-    sip.setProxyCSCF(cscf, 5060, "udp", "ipv4")
-
-    if bind: sip.setLocalIP(bind, "tcp")
-
-    # Milenage parameters
-    sip.setAMF("8000")
-    sip.setPassword(Ki)
-    sip.setOperatorIdConcealed(opc)
-
-    if(ipsec):
-        if(not sip.setIPsecPlugin(ipsec)): sys.exit(1)
-        sip.setIPSecSecAgree(True)
-        sip.setIPSecParameters("hmac-md5-96", "null", "trans", "esp")
-
-    sip.start()
-
-    return sip
+        return sip
+    return run_ue
 
 
 if __name__ == "__main__":
-    cscf = sys.argv[1]
-    domain = sys.argv[2]
-    print(cscf)
-
-    time.sleep(5)
-    s0 = run_ue(imsi = "268011200608410",
-                msisdn = "972559997790",
-                Ki = "3919F39741B626604B4BACE23ACFB094",
-                opc = "177FAD988A964A3AD0421B4693257056")
+    run = gen(cscf=sys.argv[1], domain=sys.argv[2])
+    s0 = run(imsi = os.environ["IMSI"],
+             msisdn = os.environ["MSISDN"],
+             Ki = os.environ["K"],
+             opc = os.environ["OPC"])
     time.sleep(10)
-    del s0
-    time.sleep(5)
+    #del s0
 
