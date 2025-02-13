24ff89aadf89bca002b840cfd525a150f57bc4bc
Lyubomir Yatanski <l.yatanski@gmail.com>
2025-02-13 12:02:52 +0200

    add gtpv2 s5 bearer (#39)
    * add gtpv2 s5 bearer
    
    * add user plane to gtp tunnel
    
    * configure upf tun
    
    * unify gtp & sip tests

--- gtp/go.mod
+++ gtp/go.mod
@@ -2,4 +2,12 @@ module test
 
 go 1.23.5
 
-require github.com/wmnsk/go-gtp v0.8.11
+require (
+	github.com/vishvananda/netlink v1.3.0
+	github.com/wmnsk/go-gtp v0.8.11
+)
+
+require (
+	github.com/vishvananda/netns v0.0.4 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+)
--- gtp/test.go
+++ gtp/test.go
@@ -6,19 +6,27 @@ import (
 	"net"
 	"flag"
 	"time"
+	"os/exec"
 	"context"
 	"github.com/wmnsk/go-gtp/gtpv2"
 	"github.com/wmnsk/go-gtp/gtpv2/message"
 	"github.com/wmnsk/go-gtp/gtpv2/ie"
+	"github.com/vishvananda/netlink"
 )
 
 var (
 	pgwc = flag.String("pgwc", "smf", "PGWC FQDN/IP")
 	mcc = flag.String("mcc", "001", "MCC")
 	mnc = flag.String("mnc", "01", "MNC")
+	foo string
+	gtpu *netlink.GTP
 )
 
 func CreateSessionResponse(con *gtpv2.Conn, pgw net.Addr, msg message.Message) error {
+	var msIP, upfIP string
+	var oteiU uint32
+	var addr netlink.Addr
+
 	ses, err := con.GetSessionByIMSI(fmt.Sprintf("%s%s%0.10d", *mcc, *mnc, 1))
 	if err != nil {
 		return err
@@ -38,6 +46,92 @@ func CreateSessionResponse(con *gtpv2.Conn, pgw net.Addr, msg message.Message) e
 		con.RemoveSession(ses)
 		return &gtpv2.RequiredIEMissingError{Type: ie.FullyQualifiedTEID}
 	}
+
+	if paaIE := res.PAA; paaIE != nil {
+		msIP, err = paaIE.IPAddress()
+		if err != nil {
+			return err
+		}
+	}
+
+	if brCtxIE := res.BearerContextsCreated; brCtxIE != nil {
+		for _, childIE := range brCtxIE[0].ChildIEs {
+			switch childIE.Type {
+			case ie.FullyQualifiedTEID:
+				upfIP, err = childIE.IPAddress()
+				if err != nil {
+					return err
+				}
+				oteiU, err = childIE.TEID()
+			}
+		}
+	}
+
+	log.Println("MS IP", msIP)
+	pdp := &netlink.PDP{
+		Version:     1,
+		PeerAddress: net.ParseIP(upfIP),
+		MSAddress:   net.ParseIP(msIP),
+		OTEI:        oteiU,
+		ITEI:        uint32(1),
+	}
+
+	if err := netlink.GTPPDPAdd(gtpu, pdp); err != nil {
+		log.Fatal("8 ", err)
+	}
+
+	link, err := netlink.LinkByName("eth0")
+	if err != nil {
+		log.Fatal("x ", err)
+	}
+
+	addr.IPNet = &net.IPNet{IP: net.ParseIP(msIP), Mask: net.CIDRMask(24, 32)}
+	if err := netlink.AddrAdd(link, &addr); err != nil {
+		log.Fatal("9 ", err)
+	}
+
+	route := &netlink.Route{
+		Dst:       &net.IPNet{IP: net.IPv4zero, Mask: net.CIDRMask(0, 32)}, // default
+		LinkIndex: gtpu.Attrs().Index,
+		Scope:     netlink.SCOPE_LINK,
+		Protocol:  4,
+		Priority:  1,
+		Table:     1001,
+	}
+	if err := netlink.RouteReplace(route); err != nil {
+		log.Fatal("10 ", err)
+	}
+
+	rules, err := netlink.RuleList(0)
+	if err != nil {
+		log.Fatal("11 ", err)
+	}
+
+	mask32 := &net.IPNet{IP: net.ParseIP(msIP), Mask: net.CIDRMask(32, 32)}
+	for _, r := range rules {
+		if r.Src == mask32 && r.Table == 1001 {
+			return nil // Rule already exists, no need to add
+		}
+	}
+
+	rule := netlink.NewRule()
+	//rule.IifName = "gtp0"
+	rule.Src = mask32
+	rule.Table = 1001
+
+	if err := netlink.RuleAdd(rule); err != nil {
+		log.Fatal("12 ", err)
+	}
+
+	log.Println("before starting process")
+	cmd := exec.Command("python3", "/opt/test.py")
+	out, err := cmd.Output()
+	if err != nil {
+		return err
+	}
+	log.Println(string(out))
+	log.Println("process finished")
+
 	return nil
 }
 
@@ -50,15 +144,22 @@ func CreateBearerRequest(con *gtpv2.Conn, pgw net.Addr, msg message.Message) err
 	if err != nil {
 		return err
 	}
+	req := msg.(*message.CreateBearerRequest)
 	res := message.NewCreateBearerResponse(
-		teid, 2,
+		teid, req.SequenceNumber,
 		ie.NewCause(gtpv2.CauseRequestAccepted, 0, 0, 0, nil),
 		ie.NewBearerContext(
+			ie.NewCause(gtpv2.CauseRequestAccepted, 0, 0, 0, nil),
 			ie.NewEPSBearerID(5),
+			ie.NewFullyQualifiedTEID(gtpv2.IFTypeS5S8SGWGTPU, uint32(1), foo, ""),
 			ie.NewBearerQoS(1, 1, 1, 1, 0x52, 0x52, 0x52, 0x52),
 		),
 	)
-	_, err = con.SendMessageTo(res, pgw)
+	payload, err := message.Marshal(res)
+	if err != nil {
+		return err
+	}
+	_, err = con.WriteTo(payload, pgw)
 	return err
 }
 
@@ -70,31 +171,73 @@ func DeleteSessionResponse(con *gtpv2.Conn, pgw net.Addr, msg message.Message) e
 	return nil
 }
 
-func ResolveLocalAddr() (*net.UDPAddr, error) {
-	con, err := net.Dial("udp", *pgwc+gtpv2.GTPCPort)
+func ResolveLocalAddr(port string) string {
+	con, err := net.Dial("udp", *pgwc+port)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer con.Close()
-	ip := con.LocalAddr().(*net.UDPAddr)
-	return ip, err
+	return con.LocalAddr().(*net.UDPAddr).IP.String()
 }
 
 func main() {
+	flag.Parse()
 	ctx := context.Background()
 	pgw, err := net.ResolveUDPAddr("udp", *pgwc+gtpv2.GTPCPort)
 	if err != nil {
 		log.Fatal(err)
 	}
-	bind, err := ResolveLocalAddr()
-	if err != nil {
-		log.Fatal(err)
-	}
+	foo = ResolveLocalAddr(gtpv2.GTPCPort)
+	bind, err := net.ResolveUDPAddr("udp", "0.0.0.0"+gtpv2.GTPCPort)
 	con, err := gtpv2.Dial(ctx, bind, pgw, gtpv2.IFTypeS5S8SGWGTPC, 0)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("0 ", err)
 	}
 	defer con.Close()
+
+	usr0, err := net.ListenPacket("udp", "0.0.0.0:3386")
+	if err != nil {
+		log.Fatal("1 ", err)
+	}
+	defer usr0.Close()
+
+	f0, err := usr0.(*net.UDPConn).File()
+	if err != nil {
+		log.Fatal("3 ", err)
+	}
+
+	usr1, err := net.ListenPacket("udp", "0.0.0.0"+gtpv2.GTPUPort)
+	if err != nil {
+		log.Fatal("2 ", err)
+	}
+	defer usr1.Close()
+
+	f1, err := usr1.(*net.UDPConn).File()
+	if err != nil {
+		log.Fatal("4 ", err)
+	}
+
+	gtpu = &netlink.GTP{
+		LinkAttrs: netlink.LinkAttrs{
+			Name: "gtp0",
+		},
+		FD0:  int(f0.Fd()),
+		FD1:  int(f1.Fd()),
+		Role: 1,
+	}
+
+	if err := netlink.LinkAdd(gtpu); err != nil {
+		log.Fatal("5 ", err)
+	}
+	//defer netlink.LinkDel(gtpu)
+
+	if err := netlink.LinkSetUp(gtpu); err != nil {
+		log.Fatal("6 ", err)
+	}
+
+	if err := netlink.LinkSetMTU(gtpu, 1500); err != nil {
+		log.Fatal("7 ", err)
+	}
 
 	con.AddHandlers(map[uint8]gtpv2.HandlerFunc{
 		message.MsgTypeCreateSessionResponse: CreateSessionResponse,
@@ -108,8 +251,13 @@ func main() {
 		ie.NewIMSI(fmt.Sprintf("%s%s%0.10d", *mcc, *mnc, 1)),
 		ie.NewServingNetwork(*mcc, *mnc),
 		ie.NewRATType(gtpv2.RATTypeEUTRAN),
-		ie.NewFullyQualifiedTEID(gtpv2.IFTypeS5S8SGWGTPC, uint32(1), bind.IP.String(), ""),
+		ie.NewFullyQualifiedTEID(gtpv2.IFTypeS5S8SGWGTPC, uint32(1), foo, ""),
 		ie.NewAccessPointName("ims"),
+		ie.NewProtocolConfigurationOptions(
+			gtpv2.ConfigProtocolPPPWithIP,
+			ie.NewPCOContainer(gtpv2.ProtoIDIPCP, []byte{0x01, 0x00, 0x00, 0x10, 0x03, 0x06, 0x01, 0x01, 0x01, 0x01, 0x81, 0x06, 0x02, 0x02, 0x02, 0x02}),
+			ie.NewPCOContainer(0x000c, nil),
+		),
 		ie.NewSelectionMode(gtpv2.SelectionModeMSorNetworkProvidedAPNSubscribedVerified),
 		ie.NewPDNType(gtpv2.PDNTypeIPv4),
 		ie.NewPDNAddressAllocation("0.0.0.0"),
@@ -121,7 +269,7 @@ func main() {
 		),
 		ie.NewBearerContext(
 			ie.NewEPSBearerID(5),
-			ie.NewFullyQualifiedTEID(gtpv2.IFTypeS5S8SGWGTPU, uint32(1), bind.IP.String(), "").WithInstance(2),
+			ie.NewFullyQualifiedTEID(gtpv2.IFTypeS5S8SGWGTPU, uint32(1), foo, "").WithInstance(2),
 			ie.NewBearerQoS(1, 2, 1, 0xff, 0, 0, 0, 0),
 		),
 	)
@@ -129,15 +277,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	time.Sleep(8 * time.Second)
+	log.Println(ses)
+	//time.Sleep(8 * time.Second)
 
-	teid, err := ses.GetTEID(gtpv2.IFTypeS5S8PGWGTPC)
-	if err != nil {
-		log.Fatal(err)
-	}
-	con.DeleteSession(
-		teid,
-		ses,
-		ie.NewEPSBearerID(ses.GetDefaultBearer().EBI),
-	)
+	//teid, err := ses.GetTEID(gtpv2.IFTypeS5S8PGWGTPC)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//con.DeleteSession(
+	//	teid,
+	//	ses,
+	//	ie.NewEPSBearerID(ses.GetDefaultBearer().EBI),
+	//)
+	time.Sleep(300 * time.Second)
 }
