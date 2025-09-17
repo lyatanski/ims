039a82511681b0a9ab38ab4d625ea2b13ad32ad4
Lyubomir Yatanski <l.yatanski@gmail.com>
2025-09-17 16:09:16 +0300

    add QoS for test (#91)
    * add QoS for test (precondition supported)
    
    * disable DB for scscf to reduce failure cases

--- images/test/src/sip.py
+++ images/test/src/sip.py
@@ -3,8 +3,36 @@
 import os
 import sys
 import time
+import logging
 import threading
 import tinyWRAP as s
+
+class DDebugCallback(s.DDebugCallback):
+    def __init__(self):
+        super().__init__()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def OnDebugInfo(self, msg):
+        logging.info(msg)
+        return 0
+
+    def OnDebugWarn(self, msg):
+        logging.warning(msg)
+        return 0
+
+    def OnDebugError(self, msg):
+        logging.error(msg)
+        return 0
+
+    def OnDebugFatal(self, msg):
+        logging.critical(msg)
+        return 0
+
 
 ct = None
 def talk(call):
@@ -21,11 +49,11 @@ class SipCallback(s.SipCallback):
         self.__disown__()
 
     def OnDialogEvent(self, event):
-        print(f"dialog")
+        logging.info(f"dialog")
         return 0
 
     def OnStackEvent(self, event):
-        print(f"stack {event.getStack()} {event.getPhrase()}")
+        logging.info(f"stack {event.getStack()} {event.getPhrase()}")
         if event.getPhrase() == "Stack stopped":
             pass
         if event.getPhrase() == "Stack started":
@@ -35,41 +63,41 @@ class SipCallback(s.SipCallback):
         return 0
 
     def OnInviteEvent(self, event):
-        print(f"invite {event.getStack()} type {event.getType()}")
+        logging.info(f"invite {event.getStack()} type {event.getType()}")
         msg = event.getSipMessage()
         if not msg: return 0
         if not msg.isResponse():
-            print(f"should accept invite on session {event.getSession()}")
+            logging.info(f"should accept invite on session {event.getSession()}")
             if not event.getSession():
                 call = event.takeCallSessionOwnership()
                 call.setSessionTimer(3600, "none")
-                call.setQoS(s.tmedia_qos_stype_none, s.tmedia_qos_strength_none)
+                call.setQoS(s.tmedia_qos_strength_optional, s.tmedia_qos_strength_optional)
                 ct = threading.Timer(1, talk, (call,))
                 ct.start()
         return 0
 
     def OnMessagingEvent(self, event):
-        print(f"message")
+        logging.info(f"message")
         return 0
 
     def OnInfoEvent(self, event):
-        print(f"info")
+        logging.info(f"info")
         return 0
 
     def OnOptionsEvent(self, event):
-        print(f"options")
+        logging.info(f"options")
         return 0
 
     def OnPublicationEvent(self, event):
-        print(f"publication")
+        logging.info(f"publication")
         return 0
 
     def OnRegistrationEvent(self, event):
-        print(f"registration {event.getStack()} type {event.getType()}")
+        logging.info(f"registration {event.getStack()} type {event.getType()}")
         msg = event.getSipMessage()
         if not msg: return 0
         if msg.isResponse() and msg.getResponseCode() == 200:
-            print(f"sending subscribe")
+            logging.info(f"sending subscribe")
             sip = event.getStack()
             sip.setIMPU(f"sip:{self.msisdn}@{self.domain}")
             #sip.setRealm(f"sip:{self.msisdn}@{self.domain}")
@@ -82,7 +110,7 @@ class SipCallback(s.SipCallback):
         return 0
 
     def OnSubscriptionEvent(self, event):
-        print(f"subscription {event.getStack()} type {event.getType()}")
+        logging.info(f"subscription {event.getStack()} type {event.getType()}")
         return 0
 
 
@@ -126,40 +154,43 @@ if __name__ == "__main__":
     parser.add_argument('cscf', help="CSCF IP address")
     args = parser.parse_args()
 
-    print(" ".join(sys.argv))
+    logging.basicConfig(level=logging.INFO, format=f"%(levelname)8s {args.imsi}:%(message)s")
+    logging.info(" ".join(sys.argv))
 
-    run = gen(cscf=args.cscf, domain=os.environ["REALM"], ipsec=os.environ["IPSEC"])
-    s0 = run(imsi = args.imsi,
-             msisdn = args.msisdn,
-             Ki = os.environ["K"],
-             opc = os.environ["OPC"],
-             bind = args.bind)
+    with DDebugCallback():
+        run = gen(cscf=args.cscf, domain=os.environ["REALM"], ipsec=os.environ["IPSEC"])
+        s0 = run(imsi = args.imsi,
+                 msisdn = args.msisdn,
+                 Ki = os.environ["K"],
+                 opc = os.environ["OPC"],
+                 bind = args.bind)
 
-    time.sleep(6)
-
-    if args.call:
-        #s0.removeHeader("Event")
-        call = s.CallSession(s0)
-        #call.set100rel(True)
-        #call.setSessionTimer(3600, "none")
-        #call.setQoS(s.tmedia_qos_stype_none, s.tmedia_qos_strength_none)
-        call.call(f'sip:{args.call}@{os.environ["REALM"]};user=phone', s.twrap_media_audio)
-
-        #time.sleep(10)
-        #call.hold()
-        #time.sleep(10)
-        #call.resume()
-
-        time.sleep(60)
-        call.hangup()
-    else:
         time.sleep(6)
-        if ct: ct.join(60)
-        else: time.sleep(60)
 
-    time.sleep(30)
+        if args.call:
+            #s0.removeHeader("Event")
+            call = s.CallSession(s0)
+            #call.set100rel(True)
+            #call.setSessionTimer(3600, "none")
+            call.setQoS(s.tmedia_qos_strength_optional, s.tmedia_qos_strength_optional)
+            call.call(f'sip:{args.call}@{os.environ["REALM"]};user=phone', s.twrap_media_audio)
 
-    del s0
-    # wait for OK on REGISTER Expires: 0
+            #time.sleep(10)
+            #call.hold()
+            #time.sleep(10)
+            #call.resume()
+
+            time.sleep(60)
+            call.hangup()
+        else:
+            time.sleep(6)
+            if ct: ct.join(60)
+            else: time.sleep(60)
+
+        time.sleep(30)
+
+        del s0
+        # wait for OK on REGISTER Expires: 0
+
     time.sleep(3)
 
